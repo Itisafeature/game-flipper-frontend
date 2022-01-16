67139318d2beaae073f4c606f5a6da469b4a3a72
Itisafeature <thebigdebugger@gmail.com>
2022-01-15 23:01:48 -0500

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    navlinks: {
+        marginLeft: theme.spacing(10),
+        display: "flex",
+    },
+    logo: {
+        flexGrow: "1",
+        cursor: "pointer",
+    },
+    link: {
+        textDecoration: "none",
+        color: "white",
+        fontSize: "20px",
+        marginLeft: theme.spacing(20),
+        "&:hover": {
+            color: "yellow",
+            borderBottom: "1px solid white",
+        },
+    },
+}));
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <CssBaseline />
+            <Toolbar>
+                <Typography variant="h4" className={classes.logo}>
+                    <a href="#">Game-Flipper</a>
+                </Typography>
+                <div className={classes.navlinks}>
+                    <a href="#">Link</a>
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+}
+export default Navbar;
