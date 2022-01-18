d26cdcf9382f5012598297290784dc359a89a39a
Itisafeature <thebigdebugger@gmail.com>
2022-01-18 10:06:01 -0500

    removed unnecessary imports from app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from "react";import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import React from "react";
+import {Route, Routes} from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  return (
-      <div>
-       <Navbar />
-          <Routes>
-              <Route path="/" element={<div>Hello World</div>} />
-              <Route path="home" element={<Home />}/>
-          </Routes>
-      </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<div>Hello World</div>}/>
+                <Route path="home" element={<Home/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
